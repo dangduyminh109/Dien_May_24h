4ed3d9086d2b4e819fa20789ee7becc715def0fe
dangduyminh109 <dangduyminh109@gmail.com>
2025-02-18 22:33:25 +0700

    update navbar client

--- controller/client/Home.controller.js
+++ controller/client/Home.controller.js
@@ -1,7 +1,13 @@
-class homeController{
-    show(req, res) {
-        res.render("./client/page/home/",{
-            pageTitle: "Điện Máy 24h"
+const Product = require("../../models/product.model.js");
+const ProductCategory = require("../../models/product-category.model.js");
+const getCategoryTree = require("../../helpers/get-category-tree.helper.js");
+
+class homeController {
+    async show(req, res) {
+        const categoryTree = await getCategoryTree();
+        res.render("./client/page/home/", {
+            pageTitle: "Điện Máy 24h",
+            categoryTree,
         });
     }
 }
--- public/client/js/scrips.js
+++ public/client/js/scrips.js
@@ -1,33 +1,60 @@
-const category_btn__prev = document.getElementById("category-btn__prev");
-const category_btn__next = document.getElementById("category-btn__next");
-const category = document.querySelector(".category");
-const list_category = document.getElementById("list-category");
-let currentPosition = 0;
-let step = 200;
-category_btn__next.onclick = () => {
-    if (
-        category.offsetWidth + currentPosition + step <
-        list_category.scrollWidth
-    ) {
-        currentPosition += step;
-        list_category.style.transform = `translateX(-${currentPosition}px)`;
-    } else if (
-        list_category.scrollWidth - (category.offsetWidth + currentPosition) >
-        0
-    ) {
-        // do bị che 1 phần nên cộng 5 để show được hết item_category
-        currentPosition +=
-            list_category.scrollWidth -
-            (category.offsetWidth + currentPosition) + 5;
-        list_category.style.transform = `translateX(-${currentPosition}px)`;
+function handleNavbar() {
+    const category_btn__prev = document.getElementById("category-btn__prev");
+    const category_btn__next = document.getElementById("category-btn__next");
+    const category = document.getElementById("category");
+    const list_category = document.getElementById("list-category");
+    const categoryBtn = document.getElementById("category-group-btn");
+
+    let currentPosition = 0;
+    let step = 200;
+
+    if (category) {
+        if (category.offsetWidth < list_category.scrollWidth) {
+            categoryBtn.style.display = "flex";
+        } else {
+            categoryBtn.style.display = "none";
+        }
     }
-};
-category_btn__prev.onclick = () => {
-    if (currentPosition > 0 && currentPosition - step >= 0) {
-        currentPosition -= step;
-        list_category.style.transform = `translateX(-${currentPosition}px)`;
-    } else if (currentPosition > 0 && currentPosition - step < 0) {
-        currentPosition = 0;
-        list_category.style.transform = `translateX(-${currentPosition}px)`;
+    if (category_btn__next) {
+        category_btn__next.onclick = () => {
+            if (
+                list_category.scrollWidth +
+                    currentPosition -
+                    category.offsetWidth >=
+                200
+            ) {
+                currentPosition -= step;
+                list_category.style.transform = `translateX(${currentPosition}px)`;
+            } else if (
+                list_category.scrollWidth +
+                    currentPosition -
+                    category.offsetWidth >=
+                0
+            ) {
+                var remainder =
+                    list_category.scrollWidth +
+                    currentPosition -
+                    category.offsetWidth;
+                currentPosition -= remainder;
+                list_category.style.transform = `translateX(${currentPosition}px)`;
+            }
+        };
     }
-};
+    if (category_btn__prev) {
+        category_btn__prev.onclick = () => {
+            if (currentPosition <= -200) {
+                currentPosition += step;
+                list_category.style.transform = `translateX(${currentPosition}px)`;
+            } else if (currentPosition < 0) {
+                currentPosition = 0;
+                list_category.style.transform = `translateX(${currentPosition}px)`;
+            }
+        };
+    }
+}
+
+// Initialize all handlers
+function init() {
+    handleNavbar();
+}
+document.addEventListener("DOMContentLoaded", init);
